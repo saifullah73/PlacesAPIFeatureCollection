8cf86aa69d64b76bf83e01f5bfedbb0af339c894
freerunner73 <saifullakhaliq@gmail.com>
2019-07-13 16:06:28 +0500

    Implemented Nearby Search Method

--- NearbyFinder.py
+++ NearbyFinder.py
@@ -8,6 +8,7 @@ class NearbyFinder:
     outerRadius = 0
     locationsFound = []
     nearbyLocations = {}
+    totalAPICalls = 0
     def __init__(self,circle, featureset,key):
         self.circle = circle
         self.featureSet = featureset
@@ -21,27 +22,39 @@ class NearbyFinder:
         response = requests.get(url = URL, params = params)
         # gmaps = googlemaps.Client(key = key)
         # response = gplaces.places_nearby(gmaps, location=location, type=type,rank_by='distance')
-        results = response.json()
-        pprint.pprint(results)
-        if len(results['results']) > 0:
-            lat = results['results'][0]['geometry']['location']['lat']
-            lng = results['results'][0]['geometry']['location']['lng']
-            location = str(lat) +','+ str(lng)
-            return  location
+        mResults = response.json()
+        pprint.pprint(mResults)
+        results = mResults.get('results')
+        if (results is not None ) and  (len(results) > 0):
+            for x in range (len(results)):
+                lat = results[x]['geometry']['location']['lat']
+                lng = results[x]['geometry']['location']['lng']
+                location = str(lat) +','+ str(lng)
+                self.locationsFound.append(location)
         else:
-            return "none"
+            self.locationsFound.clear()
+            print("No results found in circle")
 
     def findStraightLineDistance(self, location, nearbypoint):
         ## need to implement
         print("no")
 
     def doNearbySearchForOneType(self):
+        ## for origin
+        currentDistance = 0
+        for nearbyPoint in self.locationsFound:
+            distance = self.findStraightLineDistance(self.circle.origin, nearbyPoint)
+            if distance < currentDistance:
+                currentDistance = distance
+        self.nearbyLocations[self.circle.origin] = [currentDistance, 1]
+         ##for population
         for loc in self.circle.population:
             if  self.nearbyLocations.get(loc) != None:
-                self.nearbyLocations[loc][1]+=1  ## appending by 1 to indicate that multiple loc had same coordinates , so their nearbyPoint is same
+                self.nearbyLocations[loc][1] +=1  ## appending by 1 to indicate that multiple loc had same coordinates , so their nearbyPoint is same
+                continue
             currentDistance = 0
-            for neabyPoint in self.locationsFound:
-                distance = self.findStraightLineDistance(loc,neabyPoint)
+            for nearbyPoint in self.locationsFound:
+                distance = self.findStraightLineDistance(loc,nearbyPoint)
                 if distance < currentDistance:
                     currentDistance = distance
             self.nearbyLocations[loc] = [currentDistance,1]
@@ -54,8 +67,12 @@ class NearbyFinder:
 
     def start(self):
         for type in self.featureSet.features:
-            self.searchAllPointsInOuterRadius(self.key,type,self.circle.origin)
-            self.doNearbySearchForOneType()
+            if self.totalAPICalls <= 200:
+                self.searchAllPointsInOuterRadius(self.key,type,self.circle.origin)
+                self.totalAPICalls +=1
+                self.doNearbySearchForOneType()
+            else:
+                print("limit exceeded")
 
 
 
